610e99ced9e2adee2d721a7ef77ff40535967640
Aaron Suggs <aaron@ktheory.com>
2014-04-15 14:35:45 -0400

    debug both dalli, connection pool

--- spec/integration/rack_attack_cache_spec.rb
+++ spec/integration/rack_attack_cache_spec.rb
@@ -20,7 +20,7 @@ describe Rack::Attack::Cache do
     #ActiveSupport::Cache::MemoryStore.new,
     #ActiveSupport::Cache::DalliStore.new("localhost"),
     #ActiveSupport::Cache::RedisStore.new("localhost"),
-    #Dalli::Client.new,
+    Dalli::Client.new,
     ConnectionPool.new { Dalli::Client.new },
     #Redis::Store.new
   ]
